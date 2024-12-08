de096b32af74cce71ca68cd521af0fcfb5cb4576
Wonjoon Seol <wonjoon.seol@gmail.com>
2024-12-08 21:16:38 +0900

    Add q7

---
+++ q7/part1/main.py
@@ -0,0 +1,48 @@
+filename = "q7/part1/input"
+# filename = "q7/part1/test"
+
+from collections import defaultdict
+
+
+def is_possible(arr, target):
+    # O(n * t) O(t)
+    dp = defaultdict(bool)
+    dp[0] = True
+    for num in arr:
+        next_dp = defaultdict(int)
+        for t, val in dp.items():
+            next_dp[t + num] |= val
+            next_dp[t * num] |= val
+        dp = next_dp
+    return dp[target]
+
+    # top-down time O(n * t) space O(n * t)
+    # cache = {}
+
+    # def recur(i, curr):
+    #     if curr == target and i == len(arr):
+    #         return True
+    #     if curr != target and i >= len(arr):
+    #         return False
+    #     if (i, curr) in cache:
+    #         return cache[(i, curr)]
+        
+    #     val = arr[i]
+    #     res = recur(i + 1, curr * val) or recur(i + 1, curr + val)
+    #     cache[(i, curr)] = res
+    #     return res
+    # return recur(0, 0)
+
+
+res = 0
+with open(filename) as f:
+    while line := f.readline():
+        arr = line.split(" ")
+        ans = int(arr[0][:-1])
+        arr = arr[1:]
+        arr = list(map(int, arr))
+        if is_possible(arr, ans):
+            res += ans
+
+print(res)  # 3245122495150
+
---
+++ q7/part2/main.py
@@ -0,0 +1,75 @@
+filename = "q7/part1/input"
+# filename = "q7/part1/test"
+
+from collections import defaultdict
+from math import log10, ceil, fmod
+
+
+def is_possible(arr, target):
+    cache = {}
+
+    def recur(i, curr):
+        if curr == target and i == len(arr):
+            return True
+        if curr != target and i >= len(arr):
+            return False
+        if (i, curr) in cache:
+            return cache[(i, curr)]
+        
+        val = arr[i]
+        res = recur(i + 1, curr * val) or recur(i + 1, curr + val)
+
+        if curr > 0:
+            # res |= recur(i + 1, (10 ** ceil(len(str(val)))) * curr + val) # correct
+            # res |= recur(i + 1, (10 ** ceil(log(val, 10))) * curr + val)  # wrong 97164197267598
+            res |= recur(i + 1, (10 ** ceil(log10(val))) * curr + val)   # wrong 97164197267598
+            # res |= recur(i + 1, int(f"{curr}{val}")) # correct
+ 
+        cache[(i, curr)] = res
+        return res
+    return recur(0, 0)
+
+
+def is_possible_dp(arr, target):
+    dp = defaultdict(bool)
+    dp[0] = True
+    for num in arr:
+        next_dp = defaultdict(bool)
+        for t, val in dp.items():
+            # +, *, concat
+            next_dp[t + num] |= val
+            next_dp[t * num] |= val
+            next_dp[int(f"{t}{num}")] |= val
+        dp = next_dp 
+    return dp[target]
+
+
+def is_possible_backward(arr, target):
+    dp = defaultdict(bool)
+    dp[target] = True
+    for num in reversed(arr):
+        next_dp = defaultdict(bool)
+        for t, val in dp.items():
+            # +, *, concat
+            next_dp[t - num] |= val
+            if t % num == 0:
+                next_dp[t // num] |= val
+            tens = 10 ** len(str(num)) 
+            if fmod(t, tens) == num:
+                next_dp[t // tens] |= val
+        dp = next_dp 
+    return dp[0]
+
+
+res = 0
+with open(filename) as f:
+    while line := f.readline():
+        arr = line.split(" ")
+        ans = int(arr[0][:-1])
+        arr = arr[1:]
+        arr = list(map(int, arr))
+        if is_possible_backward(arr, ans):
+            res += ans
+
+print(res)  # 105517128211543
+
