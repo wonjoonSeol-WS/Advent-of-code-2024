77c2e32843af1ad65d738e680c52f87be0f5462a
Wonjoon Seol <wonjoon.seol@gmail.com>
2024-12-18 19:54:50 +0900

    Add q4

---
+++ q4/part1.py
@@ -0,0 +1,41 @@
+TARGET = "XMAS"
+
+
+def get_input(filename):
+    grid = []
+    with open(filename, "r") as f:
+        while line := f.readline().strip():
+            row = []
+            for char in line:
+                row.append(char)
+            grid.append(row)
+    return grid
+
+
+def find_words(grid):
+    m = len(grid)
+    n = len(grid[0])
+
+    def helper(i, j, word_idx, dx, dy):
+        while True:
+            if i not in range(m) or j not in range(n) or grid[i][j] != TARGET[word_idx]:
+                return 0
+            if word_idx == len(TARGET) - 1:
+                return 1
+            i += dx
+            j += dy
+            word_idx += 1
+
+    ret = 0
+    for i in range(m):
+        for j in range(n):
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
+                ret += helper(i, j, 0, dx, dy)
+    return ret
+
+if __name__ == "__main__":
+    grid = get_input("q4/test") 
+    print(find_words(grid))
+
+    grid = get_input("q4/input") 
+    print(find_words(grid))
---
+++ q4/part2.py
@@ -0,0 +1,47 @@
+TARGET = "XMAS"
+
+
+def get_input(filename):
+    grid = []
+    with open(filename, "r") as f:
+        while line := f.readline().strip():
+            row = []
+            for char in line:
+                row.append(char)
+            grid.append(row)
+    return grid
+
+expected = {"M", "S"} 
+
+def find_words(grid):
+    m = len(grid)
+    n = len(grid[0])
+
+    def helper(i, j):
+        expected = {"M", "S"} 
+        diag_one = set()
+        diag_two = set()
+        diag_one.add(grid[i - 1][j - 1])
+        diag_one.add(grid[i + 1][j + 1])
+
+        diag_two.add(grid[i - 1][j + 1])
+        diag_two.add(grid[i + 1][j - 1])
+        # print(diag_one, diag_one == expected, diag_two, diag_two == expected)
+        if diag_one == expected and diag_two == expected:
+            return 1
+        return 0
+
+    ret = 0
+    for i in range(1, m - 1):
+        for j in range(1, n - 1):
+            if grid[i][j] != "A":
+                continue
+            ret += helper(i, j)
+    return ret
+
+if __name__ == "__main__":
+    grid = get_input("q4/test") 
+    print(find_words(grid))
+
+    grid = get_input("q4/input") 
+    print(find_words(grid))
